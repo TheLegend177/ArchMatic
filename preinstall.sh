b9f544afadd77d230a1905c8fa9a3042878a1380
Carsten Gramms <carstengramms@gmail.com>
2021-05-17 18:53:07 +0200

    small fix

--- preinstall.sh
+++ preinstall.sh
@@ -12,7 +12,7 @@ echo "     Setting up mirrors for optimal download"
 echo "-------------------------------------------------"
 timedatectl set-ntp true
 pacman -S --noconfirm pacman-contrib reflector
-if [[ -f /etc/pacman.d/mirrorlist]]; then
+if [ -f /etc/pacman.d/mirrorlist ]; then
         mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
     fi
 #curl -s "https://www.archlinux.org/mirrorlist/?country=DE&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist

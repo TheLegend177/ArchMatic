20b95a9a497011a3ca75b1b44eb41243c4ee4d26
Carsten Gramms <carstengramms@gmail.com>
2021-05-19 20:55:47 +0200

    switch to manual user passwd

--- archmatic.sh
+++ archmatic.sh
@@ -59,7 +59,7 @@ function setup {
     echo "-------------------------------------------------"
     echo "     Setting up mirrors for optimal download"
     echo "-------------------------------------------------"
-    pacman -S --noconfirm pacman-contrib reflector
+    sudo pacman -S --noconfirm pacman-contrib reflector
     if [ -f /etc/pacman.d/mirrorlist ]; then
         mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
     fi
@@ -76,17 +76,17 @@ function setup {
     echo "Changing the compression settings for "$nc" cores."
     sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g' /etc/makepkg.conf
 
-    # echo "-------------------------------------------------"
-    # echo "     Setup Language to DE and set locale"
-    # echo "-------------------------------------------------"
-    # sed -i 's/^#de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/' /etc/locale.gen
-    # locale-gen
-    # timedatectl --no-ask-password set-timezone Europe/Berlin
-    # timedatectl --no-ask-password set-ntp 1
-    # localectl --no-ask-password set-locale LANG="de_DE.UTF-8" LC_COLLATE="de_DE.UTF-8" LC_TIME="de_DE.UTF-8"
+    echo "-------------------------------------------------"
+    echo "     Setup Language to DE and set locale"
+    echo "-------------------------------------------------"
+    sed -i 's/^#de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/' /etc/locale.gen
+    locale-gen
+    timedatectl --no-ask-password set-timezone Europe/Berlin
+    timedatectl --no-ask-password set-ntp 1
+    localectl --no-ask-password set-locale LANG="de_DE.UTF-8" LC_COLLATE="de_DE.UTF-8" LC_TIME="de_DE.UTF-8"
 
-    # # Set keymaps
-    # localectl --no-ask-password set-keymap de
+    # Set keymaps
+    localectl --no-ask-password set-keymap de
 
     # Hostname
     hostnamectl --no-ask-password set-hostname $hostname
